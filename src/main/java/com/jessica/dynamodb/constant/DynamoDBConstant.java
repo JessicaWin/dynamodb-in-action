1243e6d9b26428e5a57b5e24dd59285343e67ec4
JessicaWin <baihechenlu@126.com>
2021-10-17 10:00:08 +0800

    dto model

--- src/main/java/com/jessica/dynamodb/constant/DynamoDBConstant.java
+++ src/main/java/com/jessica/dynamodb/constant/DynamoDBConstant.java
@@ -13,6 +13,9 @@ public class DynamoDBConstant {
 	public static final String GSI_TWO_NAME = "gsiTwo";
 	public static final String GSI_TWO_HASH_KEY = "gsiTwoPk";
 	public static final String GSI_TWO_RANGE_KEY = "gsiTwoSk";
+	public static final String GSI_THREE_NAME = "gsiThree";
+	public static final String GSI_THREE_HASH_KEY = "gsiThreePk";
+	public static final String GSI_THREE_RANGE_KEY = "gsiThreeSk";
 	public static final String LSI_ONE_NAME = "lsiOne";
 	public static final String LSI_ONE_RANGE_KEY = "lsiOneSk";
 	public static final String LSI_TWO_NAME = "lsiTwo";
